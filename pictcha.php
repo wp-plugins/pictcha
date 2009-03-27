a7aad662d08f3b5584e976028b214944012151e1
gilemon <gilemon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-27 19:50:41 +0000

    fully functional file (tested on Version 2.7.1)
    git-svn-id: https://plugins.svn.wordpress.org/pictcha/trunk@105760 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pictcha.php
+++ pictcha.php
@@ -42,35 +42,17 @@ function pictcha_add_option_page() {
 
 function pictcha_options_page() {
 	if(isset($_POST['pictcha-action-savekeys'])) {
-		update_option('pictcha-public-key',$_POST['pictcha-public-key']);
-		update_option('pictcha-private-key',$_POST['pictcha-private-key']);
-		echo "<div id='message' class='updated fade'><p>pictcha settings saved.</p></div>";
+		update_option('pictcha_security_level',$_POST['pictcha_security_level']);
+		echo "<div id='message' class='updated fade'><p>Pictcha settings saved.</p></div>";
     }
-	else if(isset($_POST['pictcha-action-getkeys'])) {
-		$response=file_get_contents('http://api.pictcha.com/getkeys?url='.urlencode($_POST['pictcha-url']).'&email='.urlencode($_POST['pictcha-email']));
-		$result = get_submatch('|<result>(.+)</result>|i', $response);
-		if(!empty($result)) {
-			$public_key = get_submatch('|<publickey>([\w\-]+)</publickey>|', $result);
-			$private_key = get_submatch('|<privatekey>([\w\-]+)</privatekey>|', $result);
-			if(empty($public_key) || empty($private_key)) {
-				echo "<div id='message' class='error fade'><p>Unable to get pictcha API keys ($result).</p></div>";
-			} else {
-				update_option('pictcha-public-key',$public_key);
-				update_option('pictcha-private-key',$private_key);
-				echo "<div id='message' class='updated fade'><p>pictcha API keys successfully saved. pictcha is now active!</p></div>";
-			}
-		}
-		else {
-			echo "<div id='message' class='error fade'><p>Unable to get pictcha API keys. Please contact developer@pictcha.com if this problem persists.</p></div><pre>$response</pre>";
-		}
-    }
-	$public_key = get_option('pictcha-public-key');
-	$private_key = get_option('pictcha-private-key');
-	if(empty($public_key) || empty($private_key)) {
-		echo "<div id='message' class='error fade'><p>pictcha is not yet active. Enter pictcha API keys below to make it work.</p></div>";
+	$security_level = get_option('pictcha_security_level');
+	if($security_level == "")
+	{
+		$security_level = 40;
 	}
+	
     ?>
-	<div class="wrap"><h2>pictcha Settings</h2>
+	<div class="wrap"><h2>Pictcha Settings</h2>
 	<form name="site" action="" method="post" id="pictcha-form">
 
 	<div>
@@ -79,42 +61,18 @@ function pictcha_options_page() {
 
 	<table>
 		<tr>
-			<td style="width: 100px"><label for="pictcha-public-key">Public Key:</label></td>
-			<td style="width: 350px"><input name="pictcha-public-key" id="pictcha-public-key" value="<?php echo attribute_escape($public_key); ?>" type="text" size="25" /></td>
-			<td style="width: 440px" rowspan="4">
-				<table style="border: 1px solid #777; padding-left: 5px; width: 100%">
-					<tr>
-						<th colspan="2">Get your free pictcha API keys.</th>
-					</tr>
-					<tr>
-						<td style="width: 100px"><label for="pictcha-url">URL:</label></td>
-						<td style="width: 340px"><input name="pictcha-url" id="pictcha-url" value="<?php echo attribute_escape(get_option('siteurl')); ?>" type="text" size="25" /> (required)</td>
-					</tr>
-					<tr>
-						<td><label for="pictcha-email">Email:</label></td>
-						<td><input name="pictcha-email" id="pictcha-email" value="<?php echo attribute_escape(get_option('admin_email')); ?>" type="text" size="25" /></td>
-					</tr>
-					<tr>
-						<td colspan="2" class="setting-description">We will not share your email address or spam you. It will be only used to send you API keys and occasional service updates.</td>
-					</tr>
-					<tr>
-						<td colspan="2" class="submit"><input name="pictcha-action-getkeys" id="pictcha-action-getkeys" value="Get Keys" type="submit" /></td>
-					</tr>
-				</table>				
-			</td>
-		</tr>
-		<tr>
-			<td><label for="pictcha-private-key">Private Key:</label></td>
-			<td><input name="pictcha-private-key" id="pictcha-private-key" value="<?php echo attribute_escape($private_key); ?>" type="text" size="25" /></td>
-		</tr>
-		<tr>
-			<td colspan="2" class="setting-description">Note: API keys <strong>are</strong> case sensitive.</td>
-		</tr>
-		<tr>
+			<td style="width: 120px"><label for="pictcha_security_level">Security_level :</label></td>
+			<td style="width: 350px"><input name="pictcha_security_level" id="pictcha_security_level" value="<?php echo attribute_escape($security_level); ?>" type="text" size="25" /></td>
 			<td>&nbsp;</td>
 			<td class="submit"><input name="pictcha-action-savekeys" id="pictcha-action-savekeys" type="submit" style="font-weight: bold;" value="Save Settings" /></td>
 		</tr>
 	</table>
+	<table>
+		<tr>
+			<td>Security_level = how much the picture has been indentified, higher = more tags associated<br>40 is a good balance between risk and user-friendliness<br>Maximum is 200</td>
+		</tr>
+	</table>
+	
 	
 	</fieldset>
 	</div>
@@ -149,8 +107,14 @@ function verify($llna, $llur, $ipaddress = "")
 	if($ipaddress == "")
 	{
 		$ipaddress =getenv(REMOTE_ADDR);
+		echo $ipaddress. "|";
 	}
-	$response=file_get_contents('http://utyp.net/check.php?llna='.$llna."&llur=".$llur."&lip=".$ipaddress);
+	$llna = str_replace(" ","+",$llna);
+	$llur = str_replace(" ","+",$llur);
+	$cURL = 'http://utyp.net/check.php?llna='.$llna."&llur=".$llur."&lip=".$ipaddress ;
+	echo $cURL. "|";
+	$response=file_get_contents($cURL);
+	echo $response. "|";
 	if($response == "&found=1 ")
 	{
 		return 1;
@@ -164,64 +128,43 @@ function verify($llna, $llur, $ipaddress = "")
 
 // add pictcha to the comment form
 add_action('comment_form', 'pictcha_comment_form', 10);
-function pictcha_comment_form($post_id) {
-	$public_key = get_option('pictcha-public-key');
-	if(empty($public_key)) {
-		echo "<div id='message' class='error fade'><p>pictcha is not yet active. Please enter pictcha API keys in settings.</p></div>";
+function pictcha_comment_form($post_id)
+{
+	$security_level = get_option('pictcha_security_level');
+	if($security_level == "")
+	{
+		$security_level = 40;
 	}
-	else {
+	$lapic = get_picture($security_level);
 ?>
 	<style type="text/css">
 	input#pictcha {height: auto; width: auto; border: 0}
 	#submit {display: none;}
 	</style>
-	<input type="hidden" name="pictcha_token" id="pictchatoken" value="">
-	<input type="image" name="pictcha" id="pictcha" alt="Pictcha - UTYP Engine CAPTCHA." src="">
+	<input type="hidden" name="pictureUrl" id="pictureUrl" value="<?php echo $lapic; ?>">
+	<img src="<?php echo $lapic; ?>">
+	<br> Question: <a href="http://utyp.net/pictcha-help.html" target="help">What</a>'s this picture?<br>
+	<input type="text" id="inputOr" name="inputOr" SIZE="15" maxlength="25" /><br>
+	<input type="submit" value="send" />
 
-	<script type="text/javascript">
-		function pictcha_token(token) {
-			document.getElementById('pictchatoken').value = token;
-			document.getElementById('pictcha').src = 'http://api.pictcha.com/challenge?key=<?php echo $public_key; ?>&token=' + token;
-		}
-		function pictcha_get_token() {
-			var e = document.createElement('script');
-			e.src = 'http://api.pictcha.com/token?output=json&key=<?php echo $public_key; ?>&rnd=' + Math.random();
-			e.type= 'text/javascript';
-			document.getElementsByTagName('head')[0].appendChild(e); 
-		}
-		pictcha_get_token();
-		// Firefox's bfcache workaround
-		window.onpageshow = function(e) {if(e.persisted) pictcha_get_token();};
-	</script>
 <?php
-	}
 }
 
 // verify pictcha
 add_action('preprocess_comment', 'pictcha_comment_post');
 function pictcha_comment_post($commentdata) {
-	// Ignore trackbacks
-	if($commentdata['comment_type']!='trackback') {
-		if(!isset($_POST['pictcha_x']) || !isset($_POST['pictcha_y'])) {
-			wp_die('You did not describe the image. Please go back and try again.');
-		}
-		$public_key = get_option('pictcha-public-key');
-		$private_key = get_option('pictcha-private-key');
-		if(empty($public_key) || empty($private_key)) {
-			echo "<p>pictcha is not yet active. Please enter pictcha API keys in settings.</p>";
-		}
-		else {
-			$response=file_get_contents('http://api.pictcha.com/verify?key='.$public_key.'&token='.$_POST['pictcha_token'].'&private_key='.$private_key.'&x='.$_POST['pictcha_x'].'&y='.$_POST['pictcha_y']);
-			//$result = get_submatch('|<result>(\w+)</result>|', $response);
-			if(!empty($result)) {
-				if($response!= "&found=1 ") {
-					wp_die("pictcha verification failed ($result). Please go back and try again.");
-				}
-			}
-			else {
-				wp_die('Unable to verify pictcha. Please contact the webmaster if this problem persists.'.$result);
-			}
-		}
+	
+	if(!isset($_POST['inputOr'])) 
+	{
+		wp_die('You did not describe the image. Please go back and try again.');
+	}
+	if(!isset($_POST['pictureUrl'])) 
+	{
+		wp_die('Unable to retrieve Picture. Please contact the webmaster if this problem persists.');
+	}
+	if(!verify($_POST['inputOr'], $_POST['pictureUrl']))
+	{
+		wp_die('Wrong Pictcha Answer. Try again if you\'re sure you\'re not a robot');
 	}
 	return $commentdata;
 }
